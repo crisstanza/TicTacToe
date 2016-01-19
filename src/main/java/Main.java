74fd56a013a4ad264ae365342c730f3339df60be
Helmuth Jr <muth.jr88@note.com>
2016-01-19 21:39:01 -0200

    #3 Adicionado exibição do tabuleiro no final do jogo.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import io.github.crisstanza.tictactoe.TicTacToeGame;
 import io.github.crisstanza.tictactoe.TicTacToeServer;
 
 public class Main {
+	public static final int BOARD_SIZE = 3;
 
 	private static final Config config = Config.getInstance();
 
@@ -14,6 +15,8 @@ public class Main {
 		if (game.getStatus() == TicTacToeGame.STATUS_NOT_STARTED) {
 			System.out.println("Este jogo ainda n\u00E3o iniciou.");
 		} else if (game.getStatus() == TicTacToeGame.STATUS_GAME_OVER) {
+			// System.out.println()
+			printBoard(game);
 			System.out.println("Fim do jogo.");
 		} else {
 			if (game.getTurn().equals(config.getNome())) {
@@ -23,6 +26,24 @@ public class Main {
 			}
 		}
 		//
+	}
+
+	public static void printBoard(TicTacToeGame game) {
+
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++) {
+				System.out.print(" " + game.getBoard().charAt(i) + " ");
+				if (j != BOARD_SIZE - 1) {
+					System.out.print("|");
+				} else {
+					System.out.println("");
+				}
+			}
+			if (i != BOARD_SIZE - 1) {
+				System.out.println("-----------");
+			}
+		}
+
 	}
 
 }
