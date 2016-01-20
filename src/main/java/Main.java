6912b3b53c91823674f4cfccabc960374dae5bf7
Helmuth Jr <muth.jr88@note.com>
2016-01-19 23:09:39 -0200

    Adicionados alguns mêtodos "úteis" Implementado parcialmente o tratamento do estado "RUNNIG"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,22 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import config.Config;
 import io.github.crisstanza.tictactoe.TicTacToeGame;
 import io.github.crisstanza.tictactoe.TicTacToeServer;
 
 public class Main {
 	public static final int BOARD_SIZE = 3;
-
+	public static final char SPACE = '.';
+	private static final Scanner scanner = new Scanner(System.in);
 	private static final Config config = Config.getInstance();
 
 	public static void main(String[] args) throws Exception {
 		//
-		TicTacToeServer server = new TicTacToeServer();
-		TicTacToeGame game = server.getGame();
+		clear();
+
+		final TicTacToeServer server = new TicTacToeServer();
+		final TicTacToeGame game = server.getGame();
 		//
 		if (game.getStatus() == TicTacToeGame.STATUS_NOT_STARTED) {
 			System.out.println("Este jogo ainda n\u00E3o iniciou.");
@@ -19,12 +25,32 @@ public class Main {
 			printBoard(game);
 			System.out.println("Fim do jogo.");
 		} else {
+			printBoard(game);
 			if (game.getTurn().equals(config.getNome())) {
 				System.out.println("Sua vez de jogar.");
+				System.out.print("Entre com a linha: ");
+				int row = scanner.nextInt();
+				System.out.print("Entre com a coluna: ");
+				int col = scanner.nextInt();
+				String newBoard = game.getBoard().substring(0, (row - 1) * BOARD_SIZE + (col - 1));
+				newBoard += game.getPiece();
+				newBoard += game.getBoard().substring((row - 1) * BOARD_SIZE + (col - 1) + 1);
+				System.out.println(newBoard.replace(' ', '.'));
+				System.out.println(newBoard.length());
+				game.setBoard(newBoard);
+				if (server.setGame(game).trim().equals("0")) {
+					clear();
+					printBoard(game);
+					System.out.println("Esperando a jogada do advers\u00E1rio.");
+				} else {
+					System.out.println("Jogada n\u00E3o pode ser realizada");
+				}
 			} else {
 				System.out.println("Esperando a jogada do advers\u00E1rio.");
 			}
 		}
+
+		close(scanner);
 		//
 	}
 
@@ -32,7 +58,7 @@ public class Main {
 
 		for (int i = 0; i < BOARD_SIZE; i++) {
 			for (int j = 0; j < BOARD_SIZE; j++) {
-				System.out.print(" " + game.getBoard().charAt(i) + " ");
+				System.out.print("" + SPACE + game.getBoard().charAt(BOARD_SIZE * i + j) + SPACE);
 				if (j != BOARD_SIZE - 1) {
 					System.out.print("|");
 				} else {
@@ -46,4 +72,29 @@ public class Main {
 
 	}
 
+	public static final void clear() {
+		for (int i = 0; i < 64; i++) {
+			System.out.println(".");
+		}
+	}
+
+	public static final Integer nextInt(final Scanner scanner) {
+		try {
+			return scanner.nextInt();
+		} catch (final InputMismatchException ime) {
+			return null;
+		}
+	}
+
+	public static final void close(final AutoCloseable... closeMe) {
+		try {
+			for (AutoCloseable closeable : closeMe) {
+				if (closeable != null) {
+					closeable.close();
+				}
+			}
+		} catch (final Exception exc) {
+			// nothing to do!!!
+		}
+	}
 }
